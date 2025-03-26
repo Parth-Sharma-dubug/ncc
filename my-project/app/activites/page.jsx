2ddc7c3c66c90367ac76e67b66649eb5c5935e02
Parth-Sharma-dubug <jiparthsharmaji@gmail.com>
2025-03-26 12:51:48 +0530

    added activities

---
+++ my-project/app/activites/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+// import ArchitectSection from '@/components/Architects/ArchitectSection'
+const page = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default page
